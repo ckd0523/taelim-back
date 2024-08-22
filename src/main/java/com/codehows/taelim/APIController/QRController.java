ef26b484638d7303aa9852f0c5f286164550a33f
changhyeon <ckd0523@naver.com>
2024-08-22 12:08:59 +0900

    QR조회

--- src/main/java/com/codehows/taelim/APIController/QRController.java
+++ src/main/java/com/codehows/taelim/APIController/QRController.java
@@ -1,6 +1,7 @@
 package com.codehows.taelim.APIController;
 
 import com.codehows.taelim.service.QRService;
+import com.google.zxing.WriterException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,17 +20,23 @@ public class QRController {
 
     private final QRService qrCodeService;
 
+    //QR 생성하는곳
     @GetMapping("/generateQRCode")
     public ResponseEntity<byte[]> generateQRCode(@RequestParam String assetCode) {
         try {
+            // 자산 코드에 기반하여 QR 코드를 생성합니다.
             String url = "http://localhost:8080/" + assetCode;
-            byte[] qrCode = qrCodeService.generateQRCode(url, 200, 200);
+            // String code = assetCode; // QR 코드에 포함할 텍스트를 설정합니다.
+            byte[] qrCode = qrCodeService.generateQRCode(url, 200, 200); // QR 코드 생성
 
+            // 응답 헤더를 설정합니다. 콘텐츠 타입을 PNG 이미지로 설정합니다.
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.IMAGE_PNG);
 
+            // QR 코드 바이트 배열을 응답 본문으로 설정하여 반환합니다.
             return ResponseEntity.ok().headers(headers).body(qrCode);
-        } catch (Exception e) {
+        } catch (WriterException | IOException e) {
+            // QR 코드 생성 중 오류가 발생한 경우, 500 Internal Server Error 응답을 반환합니다.
             return ResponseEntity.status(500).build();
         }
     }
--- src/main/java/com/codehows/taelim/service/QRService.java
+++ src/main/java/com/codehows/taelim/service/QRService.java
@@ -16,12 +16,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class QRService {
+
+    //QR 생성하는 서비스
     public byte[] generateQRCode(String text, int width, int height) throws WriterException, IOException {
+        // QRCodeWriter 인스턴스를 생성하여 QR 코드를 생성할 준비를 합니다.
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        // 주어진 텍스트를 바탕으로 QR 코드를 인코딩합니다.
+        // BarcodeFormat.QR_CODE는 QR 코드 형식을 의미합니다.
+        // width와 height는 QR 코드의 가로 및 세로 크기를 지정합니다.
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
 
+        // QR 코드 비트 매트릭스를 PNG 형식으로 변환하기 위해 ByteArrayOutputStream을 사용합니다.
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+
+        // MatrixToImageWriter 클래스를 사용하여 비트 매트릭스를 PNG 이미지로 변환하고
+        // ByteArrayOutputStream에 작성합니다.
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+
+        // ByteArrayOutputStream에서 바이트 배열을 추출하여 반환합니다.
+        // 이 바이트 배열은 PNG 형식의 QR 코드 이미지 데이터를 포함합니다.
         return pngOutputStream.toByteArray();
     }
 }
