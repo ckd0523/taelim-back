576092bb0f9b701f741430403599dc6fd0d42e05
ckd0523 <ckd0523@naver.com>
2024-09-09 21:52:14 +0900

    약간수정

--- src/main/java/com/codehows/taelim/repository/AssetSurveyDetailRepository.java
+++ src/main/java/com/codehows/taelim/repository/AssetSurveyDetailRepository.java
@@ -2,6 +2,7 @@ package com.codehows.taelim.repository;
 
 import com.codehows.taelim.entity.AssetSurveyDetail;
 import com.codehows.taelim.entity.AssetSurveyHistory;
+import com.codehows.taelim.entity.CommonAsset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
