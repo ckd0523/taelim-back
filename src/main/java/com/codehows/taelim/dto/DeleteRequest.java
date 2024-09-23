91d17df129d0dd876804e053328263ed7edf6d9f
JCY-3958 <w7e43958@naver.com>
2024-09-23 17:31:01 +0900

    2024-09-23 자산 조사 삭제 고침

--- src/main/java/com/codehows/taelim/dto/DeleteRequest.java
+++ src/main/java/com/codehows/taelim/dto/DeleteRequest.java
@@ -1,12 +1,14 @@
 package com.codehows.taelim.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DeleteRequest {
     //프론트에서 자산 조사 삭제를 할 때 사용하는 dto
     private List<Integer> assetSurveyNo;
