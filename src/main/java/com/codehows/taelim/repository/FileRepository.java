dad8bb7d6ff038ddcee49b827b2190bcc340bf67
name <kydea2240@naver.com>
2024-10-02 17:09:24 +0900

    241002
    파일 다운까지

--- src/main/java/com/codehows/taelim/repository/FileRepository.java
+++ src/main/java/com/codehows/taelim/repository/FileRepository.java
@@ -5,8 +5,12 @@ import com.codehows.taelim.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FileRepository extends JpaRepository<File, Long> {
 
     List<File> findByAssetNo(CommonAsset assetNo);
+
+    // fileName을 기준으로 파일을 조회하는 메서드
+    Optional<File> findByFileName(String fileName);  // UUID 기반의 fileName으로 파일 조회
 }
--- src/main/java/com/codehows/taelim/service/FileService.java
+++ src/main/java/com/codehows/taelim/service/FileService.java
@@ -7,7 +7,11 @@ import com.codehows.taelim.entity.File;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
 public interface FileService {
     File upload(MultipartFile file, CommonAsset commonAsset, FileType fileType);
     Resource getImage(String fileName);
+    // 파일 이름으로 파일 정보 조회
+    Optional<File> getFileByFileName(String fileName);  // 새로운 메서드 추가
 }
