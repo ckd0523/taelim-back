1a5287b1408934885824895f0d81eae15271d7c3
changhyeon <ckd0523@naver.com>
2024-10-10 17:51:25 +0900

    Update RepairFileService.java

--- src/main/java/com/codehows/taelim/service/RepairFileService.java
+++ src/main/java/com/codehows/taelim/service/RepairFileService.java
@@ -22,7 +22,7 @@ public class RepairFileService {
     @Value("${file.path}")
     private String filePath;
 
-    @Value("https://www.taelimasset.com/api/maintain/")
+    @Value("http://localhost:8080/maintain/")
     private String fileUrl;
 
     public RepairFileService(RepairFileRepository repairFileRepository) {
