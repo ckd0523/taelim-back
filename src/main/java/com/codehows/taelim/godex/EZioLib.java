d775ffd413f2ce2bffcccc040d78d52259142de8
changhyeon <ckd0523@naver.com>
2024-10-12 13:55:47 +0900

    Update EZioLib.java

--- src/main/java/com/codehows/taelim/godex/EZioLib.java
+++ src/main/java/com/codehows/taelim/godex/EZioLib.java
@@ -12,7 +12,7 @@ public class EZioLib
 
 	static {
 		try {
-			String libraryPath = "/gen/x64/libezio.so";
+			String libraryPath = "/gen/x64/libezio";
 			System.out.println("Attempting to load library from: " + libraryPath);
 			System.load(libraryPath);
 
@@ -20,7 +20,7 @@ public class EZioLib
 			System.setProperty("jna.debug_load", "true");
 			System.setProperty("jna.debug_load.jna", "true");
 
-			INSTANCE = Native.load("ezio", API.class);
+			INSTANCE = Native.load("libezio", API.class);
 			System.out.println("Successfully initialized JNA interface");
 		} catch (UnsatisfiedLinkError e) {
 			System.err.println("Failed to load native library: " + e.getMessage());
