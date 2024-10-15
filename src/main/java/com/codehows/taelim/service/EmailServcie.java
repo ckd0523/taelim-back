201783c4156ea74479693aa376304599cae06c97
changhyeon <ckd0523@naver.com>
2024-10-15 14:40:19 +0900

    Email

--- src/main/java/com/codehows/taelim/service/EmailServcie.java
+++ src/main/java/com/codehows/taelim/service/EmailServcie.java
@@ -2,23 +2,27 @@ package com.codehows.taelim.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class EmailServcie {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
     public void sendEmail(String to, String subject, String body) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
+        //받는 사람 이메일
         helper.setTo(to);
+        //제목
         helper.setSubject(subject);
+        //내용
         helper.setText(body, true);
 
         mailSender.send(message);
