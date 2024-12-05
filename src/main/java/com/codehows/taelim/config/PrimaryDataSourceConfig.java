123d8e2abf6627bf7594e6ff5458582a2e2d8862
JCY-3958 <w7e43958@naver.com>
2024-12-05 17:40:59 +0900

    2024-12-05 더미 데이터 넣기

--- src/main/java/com/codehows/taelim/config/PrimaryDataSourceConfig.java
+++ src/main/java/com/codehows/taelim/config/PrimaryDataSourceConfig.java
@@ -41,7 +41,7 @@ public class PrimaryDataSourceConfig {
         Map<String, Object> properties = new HashMap<>();
         properties.put("hibernate.physical_naming_strategy",
                 "org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy");
-        properties.put("hibernate.hbm2ddl.auto", "create");
+        properties.put("hibernate.hbm2ddl.auto", "validate");
 
         return builder
                 .dataSource(dataSource)
